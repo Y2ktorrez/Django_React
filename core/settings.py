1e46eb357f338992962f08fcec911967cb8f887a
Y2ktorrez <andrestorrezvaca@gmail.com>
2024-11-23 10:33:40 -0400

    Convinacion de Django con React

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,8 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #Definimos el directorio build de react
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
